37852c0d637938bb6ac25005676c53e68bce4b5e
Michael D. Johnson <mdjohnson44@gmail.com>
2015-10-19 15:01:35 -0500

    updating funcs to trim calibrations dataframe

--- python/PipeBox/nitelycal_lib.py
+++ python/PipeBox/nitelycal_lib.py
@@ -4,24 +4,36 @@ import pandas as pd
 from PipeBox import query
 
 def remove_junk(dataframe):
-    df = dataframe['PTC' not in df.object | 'junk' not in df.object]
-    return dataframe
+    df = dataframe[(dataframe.object.str.contains('PTC') ==False) | (dataframe.object.str.contains('junk')==False)]
+    return df
 
 def remove_first(dataframe):
     grouped = dataframe.groupby(by=['obstype','nite','band'])
-    for name,group in grouped.iteritems():
-        print name,group     
+    first_index_list = []
+    for name,group in grouped:
+        first_index_list.append(group.index[0])
+    new_df = dataframe.drop(dataframe.index[first_index_list])
+    return new_df
 
-def remove_rband(dataframe):
-    df = dataframe[df.band == 'r' and df.exptime <= 15.0]
-    return dataframe
+def remove_satrband(dataframe):
+    nor_df = dataframe[(dataframe.band !='r')]
+    r_df = dataframe[(dataframe.band == 'r') & (dataframe.exptime >= 15)]
+    df = pd.concat([nor_df,r_df]) 
+    return df
 
 def create_dataframe(query_object):
     df = pd.DataFrame(query_object,columns=['nite','expnum','band','exptime','obstype',
                                             'program','propid','object'])
-    junkless_df = remove_junk(df)
-    trimmed_df = remove_first(df)
+    return df
  
 cur = query.NitelyCal('db-desoper')
-query = cur.get_cals(20151017)
-   
+query = cur.get_cals(20151007)
+df = create_dataframe(query)
+print df
+new_df = remove_first(df)
+print new_df
+# Munge the dataframe
+#junkless_df = remove_junk(df)
+#trimmed_df = remove_first(junkless_df)   
+#final_df = remove_satrband(trimmed_df)
+#print final_df
