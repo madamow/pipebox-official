7ed1becf5afc234c4712606324d25063a20bcdcd
mjohns44 <mjohns44@illinois.edu>
2015-12-15 10:43:57 -0600

    renaming pipebox_utils to pipeutils

--- python/pipebox/__init__.py
+++ python/pipebox/__init__.py
@@ -12,4 +12,4 @@ env = Environment(loader=FileSystemLoader(templates_dir),trim_blocks=True)
 # for jinja2 2.7+
 #env = Environment(loader=FileSystemLoader(templates_dir),trim_blocks=True,lstrip_blocks=True)
 
-from .pipebox_utils import *
+from .pipeutils import *
