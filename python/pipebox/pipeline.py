cf87fc08b9d14b467b71451c775634ecac5e869a
Xinyang Lu <xlu28@illinois.edu>
2016-03-10 14:31:22 -0600

    Added a warning message for the situation where  both nite and niterange are specified.

--- python/pipebox/pipeline.py
+++ python/pipebox/pipeline.py
@@ -290,6 +290,8 @@ class WideField(PipeLine):
             self.args.exposure_list = self.args.expnum.split(',')
             self.args.dataframe = pd.DataFrame(self.args.exposure_list,columns=['expnum'])
         elif self.args.nite or self.args.niterange:
+            if self.args.nite and self.args.niterange:
+                print "Warning: Both nite and niterange are specified. Only nite will be used."
             if self.args.nite:
                 self.args.nite = self.args.nite.strip().split(',')
             else:
