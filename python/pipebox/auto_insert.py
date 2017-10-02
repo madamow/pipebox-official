da18abd10db4d008e5898b7453692cb5d396d950
mjohns44 <mjohns44@illinois.edu>
2017-10-02 13:00:20 -0500

    removing hard-coded test nite for autoqueue

--- python/pipebox/auto_insert.py
+++ python/pipebox/auto_insert.py
@@ -7,5 +7,5 @@ pipeline = pipequery.PipeQuery('db-desoper')
 propid,program = pipeline.get_propids_programs()
 
 # Insert exposures into mjohns44.auto_queue
-pipeline.insert_auto_queue(nites=['20171001'],program=program,propid=propid)
+pipeline.insert_auto_queue(program=program,propid=propid)
 
